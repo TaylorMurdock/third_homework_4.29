3aaebb5dd0e68eb4043aa0e2c6f9c0873bd6b9aa
Taylor Murdock <taylormurdock@Taylors-MacBook-Pro.local>
2023-04-30 23:32:22 -0400

    Methods Revisited answered

--- answers.js
+++ answers.js
@@ -71,6 +71,67 @@ for ( let str of ninjaTurtles) {
 ////////////////////////////////
 // Methods, Revisited
 ////////////////////////////////
+// Here is a list of favMovies:
+// Console log: the index of Titanic
+// Do the following and console.log the final results (I have included some thought questions, you don't have to write out an answer for those marked as such):
+// use the .sort method Thought question: what did this do to the array? Did it permanently alter it?
+// Use the method pop
+// push "Guardians of the Galaxy"
+// Reverse the array
+// Use the shift method
+// unshift - what does it return?
+// splice "Django Unchained" and add "Avatar" (try finding the index of "Django Unchained", instead of counting it yourself) Thought question: did this permanently alter our array?
+// slice the last half of the array (challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it) - Thought question: did this permanently alter our array?
+// store the value of your slice in a variable, console.log it - Thought question: what is going on here?
+// console.log your final results
+// After running the above tasks, console.log the index of "Fast and Furious" -We removed it from the array, what value do we get when we look for the index of something that is not in the array?
+// Thought question: that we declared the variable favMovies with const, and yet, we were allowed to change the array. Weird? Should we have used let?
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+
+const index = favMovies.indexOf("Titanic");
+console.log(index);
+
+favMovies.sort();
+console.log(favMovies);
+
+const index2 = favMovies.indexOf("Oculus");
+if (index2 > -1) {
+    favMovies.splice(index2, 1);
+}
+console.log(favMovies);
+
+favMovies.push("Guardians of the Galaxy");
+console.log(favMovies);
+
+favMovies.reverse();
+console.log(favMovies);
+
+favMovies.shift();
+console.log(favMovies);
+
+favMovies.unshift("The Godfather"); // this put "The Godfather back into the list and in front"
+console.log(favMovies);
+
+const index3= favMovies.indexOf("Django Unchained");
+if (index3 > -1) {
+    favMovies.splice(index3, 1); //SPLICE for removing SLICE for inserting
+    favMovies.push("Avatar");
+}
+console.log(favMovies)
+
+const middleIndex = Math.floor(favMovies.length / 2); // find the middle index
+const lastHalf = favMovies.slice(middleIndex); // creating a new array of the lst half of the OG array (half is determined by the middleIndex above)
+console.log(lastHalf); // it is only showing me the last half of the array which was given to me from my code above
+
+console.log(favMovies); // it never replaces the OG array it just prints a copy of the new array along with the OG array
+
+console.log(favMovies.indexOf("Fast and Furious")); // i got back 14 as an output and from my research that is because the console log is searching the OG array and not the altered array
+
+// Thought question: that we declared the variable favMovies with const, and yet, we were allowed to change the array. Weird? Should we have used let?
+// I was able to change the variable using const because it was an array. array or an object you can stil modifty its properties and elements 
+
+
+
 
 ////////////////////////////////
 // Where is Waldo
